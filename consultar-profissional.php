c8e94344329952df08f1ca32df58fd33971358be
Técnico em Informática 2019.4 <42121742019.4@senacrjedu.local>
2021-01-26 21:55:59 -0300

    Consulta terminada

--- consultar-profissional.php
+++ consultar-profissional.php
@@ -1,34 +1,79 @@
 <?php
-require __DIR__."/DB/Connect.php";
+require __DIR__ . "/DB/Connect.php";
 $db = Connect::getInstance();
 
 $profissoes = $db->query("SELECT * FROM profissao ORDER BY nome ASC")->fetchAll();
+$cep = "";
 
+$consulta = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRIPPED);
 
-$consulta = filter_input_array(INPUT_GET, FILTER_SANITIZE_STRIPPED);
-
-if(isset($consulta)){
+if (isset($consulta)) {
     // var_dump($consulta);
-    
-    if($consulta['profissao']){
-        
+    if (!empty($consulta['cep'])) {
+        $cep = $consulta['cep'];
+
+        if (preg_match("/^[0-9]{5}-[0-9]{3}/", $consulta['cep']) != 1) {
+            $erros['cep_formato'] = true;
+        } else {
+            $viacep = file_get_contents("https://viacep.com.br/ws/{$consulta['cep']}/json/");
+            if (!isset(json_decode($viacep)->erro)) {
+                $consulta['bairro'] = json_decode($viacep)->bairro;
+                $consulta['municipio'] = json_decode($viacep)->localidade;
+            } else {
+                $erros['cep_invalido'] = true;
+            }
+        }
+    }
+
+    if (!empty($consulta['profissao'])) {
+
         try {
             $stmt = $db->prepare("SELECT * FROM especialidade WHERE profissao_id = :id");
             $stmt->bindParam(":id", $consulta['profissao'], PDO::PARAM_INT);
             $stmt->execute();
-            
+
             $especialidades = $stmt->fetchAll();
             // var_dump($especialidades);
-            
+
             $stmt = $db->prepare("SELECT * FROM profissao WHERE id = :id");
             $stmt->bindParam(":id", $consulta['profissao'], PDO::PARAM_INT);
             $stmt->execute();
-            
+
             $profissao_selecionada = $stmt->fetch();
             // var_dump($profissao_selecionada);
-            
+
+            if (!empty($consulta['especialidade'])) {
+                $especialidade_selecionada = $db->query("SELECT * FROM especialidade WHERE id =" . $consulta['especialidade'])->fetch();
+                // var_dump($especialidade_selecionada);
+            }
         } catch (Exception $ex) {
-           echo "Deu erro tio: {$ex}";
+            echo "Deu erro tio: {$ex}";
+        }
+    }
+
+    if (!isset($erros) && !empty($consulta['cep']) && !empty($consulta['profissao']) && !empty($consulta['especialidade'])) {
+
+        try {
+            $stmt = $db->prepare("SELECT * FROM profissional WHERE municipio = :municipio AND bairro = :bairro AND profissao_id = :profissao_id AND especialidade_id = :especialidade_id");
+            $stmt->bindParam(":municipio", $consulta['municipio']);
+            $stmt->bindParam(":bairro", $consulta['bairro']);
+            $stmt->bindParam(":profissao_id", $consulta['profissao']);
+            $stmt->bindParam(":especialidade_id", $consulta['especialidade']);
+            $stmt->execute();
+            $profissionais = $stmt->fetchAll();
+        } catch (Exception $ex) {
+            "Deu ruim: " . $ex;
+        }
+    } elseif (!isset($erros) && !empty($consulta['cep']) && !empty($consulta['profissao'])) {
+        try {
+            $stmt = $db->prepare("SELECT * FROM profissional WHERE municipio = :municipio AND bairro = :bairro AND profissao_id = :profissao_id");
+            $stmt->bindParam(":municipio", $consulta['municipio']);
+            $stmt->bindParam(":bairro", $consulta['bairro']);
+            $stmt->bindParam(":profissao_id", $consulta['profissao']);
+            $stmt->execute();
+            $profissionais = $stmt->fetchAll();
+        } catch (Exception $ex) {
+            "Deu ruim: " . $ex;
         }
     }
 }
@@ -37,87 +82,145 @@ if(isset($consulta)){
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Font-->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;1,700&family=Poppins:wght@400;600&display=swap"
-        rel="stylesheet">
-    <!--CSS-->
-    <link rel="stylesheet" href="./styles/index.css" />
-    <link rel="stylesheet" href="./styles/consultar-profissional.css" />
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--Font-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link
+            href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;1,700&family=Poppins:wght@400;600&display=swap"
+            rel="stylesheet">
+        <!--CSS-->
+        <link rel="stylesheet" href="./styles/index.css" />
+        <link rel="stylesheet" href="./styles/consultar-profissional.css" />
+        <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery.mask.min.js"></script>
 
-    <title>Empresa - Consultar Profissional</title>
-</head>
+        <title>Empresa - Consultar Profissional</title>
+    </head>
 
-<body>
-    <header>
-        <a href="./index.php" class="botao-voltar"><img src="./img/arrow.svg" alt="botão voltar"></a>
-        <div class="header-content">
-            <h1>Escolha o melhor profissonal para o serviço que você deseja</h1>
-        </div>
-    </header>
-    <form action="<?= $_SERVER['PHP_SELF'] ?>" post="get">
-        <div class="occupation">
-            <label for="profissao">Profissão</label>
-            <select name="profissao" id="occupation" onchange="this.form.submit()">
-                
-                <?php if(!isset($profissao_selecionada)): ?>
-                    <option value="">Selecione uma Profissão</option>
-                <?php else: ?>
-                    <option value="<?= $profissao_selecionada->id ?>"><?= $profissao_selecionada->nome ?></option>
-                <?php endif; ?>
-                <?php foreach($profissoes as $profissao): ?>
-                    <option value="<?= $profissao->id ?>"><?= $profissao->nome ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
+    <body>
+        <header>
+            <a href="./index.php" class="botao-voltar"><img src="./img/arrow.svg" alt="botão voltar"></a>
+            <div class="header-content">
+                <h1>Escolha o melhor profissonal para o serviço que você deseja</h1>
+            </div>
+        </header>
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post" id="consulta">
+            <div class="occupation">
+                <label for="profissao">Profissão</label>
+                <select name="profissao" id="occupation" onchange="submeterProfissao()">
 
-        <div class="specialism">
-            <label for="especialidade" class="specialism">Especialidade</label>
-            <select name="especialidade" id="specialism">
-                <?php if(!isset($especialidades)): ?>
-                    <option value="">Selecione uma profissão ANTES CARALHO</option>
-                <?php else: ?>
-                    <option value="">Selecione uma especialidade</option>
-                    <?php foreach($especialidades as $especialidade): ?>
-                    <option value="<?= $especialidade->id ?>"><?= $especialidade->nome ?></option>
+                    <?php if (!isset($profissao_selecionada)): ?>
+                        <option value="">Selecione uma Profissão</option>
+                    <?php else: ?>
+                        <option value="<?= $profissao_selecionada->id ?>"><?= $profissao_selecionada->nome ?></option>
+                    <?php endif; ?>
+                    <?php foreach ($profissoes as $profissao): ?>
+                        <?php if (isset($profissao_selecionada)): ?>
+                            <?php
+                            if ($profissao->id == $profissao_selecionada->id): continue;
+                            endif;
+                            ?>
+                            <option value="<?= $profissao->id ?>"><?= $profissao->nome ?></option>
+                        <?php else: ?>
+                            <option value="<?= $profissao->id ?>"><?= $profissao->nome ?></option>
+                        <?php endif; ?>
                     <?php endforeach; ?>
-                <?php endif; ?>
-            </select>
-        </div>
+                </select>
+            </div>
 
-        <div class="cep">
-            <label for="cep" class="cep">CEP</label>
-            <input type="text" name="cep" id="cep">
-        </div>
+            <div class="specialism">
+                <label for="especialidade" class="specialism">Especialidade</label>
+                <select name="especialidade" id="especialidade">
+                    <?php if (!isset($especialidades)): ?>
+                        <option value="">---</option>
+                    <?php else: ?>
+                        <?php if (isset($especialidade_selecionada)): ?>
+                            <option value="<?= $especialidade_selecionada->id ?>"><?= $especialidade_selecionada->nome ?></option>
+                        <?php else: ?>
+                            <option value="">Todos</option>
+                        <?php endif; ?>
+                        <?php foreach ($especialidades as $especialidade): ?>
+                            <?php if (isset($especialidade_selecionada)): ?>
+                                <?php
+                                if ($especialidade->id == $especialidade_selecionada->id): continue;
+                                endif;
+                                ?>
+                                <option value="<?= $especialidade->id ?>"><?= $especialidade->nome ?></option>
+                            <?php else: ?>
+                                <option value="<?= $especialidade->id ?>"><?= $especialidade->nome ?></option>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                                <?php if(isset($especialidade_selecionada)): ?>
+                                <option value="">Todos</option>
+                                <?php endif; ?>
+                    <?php endif; ?>
+                </select>
+            </div>
+
+            <div class="cep">
+                <label for="cep" class="cep">CEP</label>
+                <input type="text" name="cep" id="cep" <?= (isset($erros['cep_formato']) or isset($erros['cep_invalido'])) ? "style='border: 1px solid red;'" : "" ?> value="<?= $cep ?>"><!-- pattern="[0-9]{5}-[0-9]{3}" title="99999-999" -->
+                <script type="text/javascript">$("#cep").mask("00000-000");</script>
+            </div>
+
+            <div class="button">
+                <button type="submit">Buscar</button>
+            </div>
+        </form>
+
         
-        <div class="button">
-            <button type="submit">Enviar</button>
-        </div>
-    </form>
+        <?php if (!isset($erros)): ?>
+            <?php if (isset($profissionais)): ?>
+                <?php foreach ($profissionais as $profissional): ?>
+        
+                <?php $tel = $db->query("SELECT * FROM telefone WHERE profissional_id = ".$profissional->id)->fetch();?>
+                <?php $prof = $db->query("SELECT * FROM profissao WHERE id = ".$profissional->profissao_id)->fetch();?>
+                <?php if(!empty($profissional->especialidade_id)){ $esp = $db->query("SELECT * FROM especialidade WHERE id = " . $profissional->especialidade_id)->fetch();} else {$esp=null;}?>
+                
+                    <div class="card">
+                        <div class="img">
+                            <img src="./img/icon/<?= $prof->icone ?>" width="100" height="100" alt="">
+                        </div>
+                        <div class="professional">
+                            <p><?= $profissional->nome ?></p>
+                            <p><?= (isset($esp->nome) and !empty($esp->nome)) ? $esp->nome : "Sem especialidade" ?></p>
+                            <p>
+                                 <?= $profissional->descricao ?>
+                            </p>
+                        </div>
+                        <div class="social-media">
+                            <div class="net whatss">
+                                <a href="https://api.whatsapp.com/send?phone=55<?= $tel->ddd . $tel->numero ?>&text=Oi%2C+<?= urlencode($profissional->nome) ?>%2C+encontrei+voc%C3%AA+pelo+Achaki.+Gostaria+de+informa%C3%A7%C3%B5es+sobre+os+seus+servi%C3%A7os."><img src="./img/icon/icon-whatsapp.svg" alt=""></a>
+                            </div>
+                            <div class="net email">
+                                <a href="mailto:<?= $profissional->email ?>"><img src="./img/icon/icon-email.svg" alt=""></a>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <div class="card">
+                    <h1>Os resultados irão aparecer aqui</h1>
+                </div>
+            <?php endif; ?>
 
-    <div class="card">
-        <div class="img">
-        <img src="" alt="">
-        </div>
-        <div class="professional">
-            <p>Matheus dos Santos Caldeira</p>
-            <p>Desing Gráfico</p>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            </p>
-        </div>
-        <div class="social-media">
-        <div class="net whatss">
-            <a href=""><img src="./img/icon/icon-whatsapp.svg" alt=""></a>
-        </div>
-        <div class="net email">
-            <a href=""><img src="./img/icon/icon-email.svg" alt=""></a>
-        </div>
-        </div>
-    </div>
-</body>
+        <?php else: ?>
+            <div class="card">
+                <h1>CEP INVÁLIDO</h1>
+            </div>
+        <?php endif; ?>
+
+        <script>
+            function submeterProfissao() {
+                let form = document.getElementById("consulta");
+                let especialidade = document.getElementById("especialidade");
+
+                especialidade.value = "";
+                form.submit();
+
+            }
+        </script>
+    </body>
 </html>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 session_start();
 
-require __DIR__."/DB/Connect.php";
+require __DIR__ . "/DB/Connect.php";
 $db = Connect::getInstance();
 
 $profissoes = $db->query("SELECT * FROM profissao ORDER BY nome ASC")->fetchAll();
@@ -11,68 +11,69 @@ $profissoes = $db->query("SELECT * FROM profissao ORDER BY nome ASC")->fetchAll(
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--Font-->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;1,700&family=Poppins:wght@400;600&display=swap" rel="stylesheet"> 
-    <!--CSS-->
-    <link rel="stylesheet" href="./styles/index.css" />
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--Font-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;1,700&family=Poppins:wght@400;600&display=swap" rel="stylesheet"> 
+        <!--CSS-->
+        <link rel="stylesheet" href="./styles/index.css" />
+        <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="js/jquery.mask.min.js"></script>
 
-    <title>Empresa - Página Inicial</title>
-</head>
+        <title>Empresa - Página Inicial</title>
+    </head>
 
-<body>
-	
-	<?php 
-	if(isset($_SESSION['msg'])):
-		echo "<div class='alert'>{$_SESSION['msg']}</p>";
-		unset($_SESSION['msg']);
-	else:
-		echo "";
-	endif;
-	?>
-    
-	<main id="container">
+    <body>
 
-        <ul>
-            <?= isset($_SESSION['usuario']) ? "" : "<li><a href='./cadastro-servico.php'>Cadastre-se</a></li>"?>
-            <li><a href="./login.php" class='border-left'><?= !isset($_SESSION['usuario']) ? "Login" : "Minha conta" ?></a></li>
-        </ul>
+        <?php
+        if (isset($_SESSION['msg'])):
+            echo "<div class='alert'>{$_SESSION['msg']}</p>";
+            unset($_SESSION['msg']);
+        else:
+            echo "";
+        endif;
+        ?>
 
-        <div class="intro-text">
-            <h1>Achakí</h1>
-            <h2>Sua plataforma de buscas de serviços online</h2>
-        </div>
+        <main id="container">
 
-        <div class="img">
-            <img src="./img/img-pag-inicial.svg" alt="">
-        </div>
+            <ul>
+                <?= isset($_SESSION['usuario']) ? "" : "<li><a href='./cadastro-servico.php'>Cadastre-se</a></li>" ?>
+                <li><a href="./login.php" class='border-left'><?= !isset($_SESSION['usuario']) ? "Login" : "Minha conta" ?></a></li>
+            </ul>
 
-        
+            <div class="intro-text">
+                <h1>Achakí</h1>
+                <h2>Sua plataforma de buscas de serviços online</h2>
+            </div>
 
-        <form action="consultar-profissional.php" class="search-container">
+            <div class="img">
+                <img src="./img/img-pag-inicial.svg" alt="">
+            </div>
 
-            <select name="occupation" id="occupation">
-                <option value="">Escolha uma profissão</option>
-                <?php
-                foreach ($profissoes as $profissao): ?>
-                    <option value="<?= $profissao->id ?>"><?= $profissao->nome ?></option>
-                <?php
-                endforeach; ?>
-            </select>
 
-            <input type="text" name="cep" placeholder="CEP">
 
-            <button>Consultar</button>
-    
-        </form>
+            <form action="consultar-profissional.php" class="search-container" method="post">
 
-        <div class="icon">
-            Ícones feitos por <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a>
-        </div>
-    </main>
-</body>
+                <select name="profissao" id="occupation">
+                    <option value="">Escolha uma profissão</option>
+                    <?php foreach ($profissoes as $profissao): ?>
+                        <option value="<?= $profissao->id ?>"><?= $profissao->nome ?></option>
+                        <?php endforeach; ?>
+                </select>
+
+                <input type="text" name="cep" id="cep" placeholder="CEP"  pattern="[0-9]{5}-[0-9]{3}" title="99999-999">
+                <script type="text/javascript">$("#cep").mask("00000-000");</script>
+
+                <button>Consultar</button>
+
+            </form>
+
+            <div class="icon">
+                Ícones feitos por <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com</a>
+            </div>
+        </main>
+    </body>
 
 </html>
